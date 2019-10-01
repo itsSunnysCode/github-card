76ded900c474b78cddbd6afabc1c4ae794fe6d8a
Himanshu Sain <sainhimanshu9@gmail.com>
2019-10-02 01:32:18 +0530

    error message and algo added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,15 @@
 import React from 'react'
 const Card=(props)=>{
-    
-    return(
+
+    if( props.notFound === 'Not Found'){
+        return(
+            <div className="content">
+            <h3>Please Enter Correct Username!!!</h3>
+            </div>
+            );
+    }
+    else{
+        return(
         <div className="content">
         <img  src ={props.avatar} alt='avatar display here' />
         <a href={props.homeURL} rel="noopener noreferrer" target='_blank' title='click here to redirect to github profile'><h2 href={props.homeURL} target='_blank' ><strong>{props.name}</strong></h2></a>
@@ -9,8 +17,10 @@ const Card=(props)=>{
         <h3 title='number of reposetories'><span>Repositories:</span><strong><h2>{props.repos}</h2></strong></h3>
         <h3 title='number of following'><span>Following:</span><strong><h2>{props.following}</h2></strong></h3>
         
-        
         </div>
     );
+    }
 }
+    
+
 export default Card
